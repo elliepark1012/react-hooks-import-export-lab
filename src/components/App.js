a7ddadd0e84d913e934e840d16dd071ec2aa6971
Ellie Park <elliepark1012@gmail.com>
2022-12-20 13:04:59 -0500

    export & import lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import About from "./About";
+// import {username, city, image} from "../data/user"
+
 
 function App() {
   return (
